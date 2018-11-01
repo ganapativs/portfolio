cef89674bd419cfcf38731fe75b6c30aa7663a8f
Ganapati V S <vsg.inbox@gmail.com>
2018-11-01 14:25:22 +0530

    Logo background

--- src/profile-logo/profile-logo.js
+++ src/profile-logo/profile-logo.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import Logo from '../logo/meetguns';
-import './profile-logo.css';
+import React from "react";
+import Logo from "../logo/meetguns";
+import "./profile-logo.css";
 
 export default React.memo(function ProfileLogo(props) {
   return (
     <div className="text-center full-width">
       <div className="user__logo animated fadeIn">
         <div className="user__logo--bg-rotate">
-          <div className="user__logo--bg" />
+          <div className="user__logo--bg">
+            <div className="user__logo--image--wrapper">
+              <div className="user__logo--image" />
+            </div>
+          </div>
         </div>
-        <Logo height={60} />
+        <div className="user__logo--svg-wrapper">
+          <Logo height={60} />
+        </div>
       </div>
     </div>
   );
