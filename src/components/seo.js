8b015ffbc9d01979f05f68f2c1d48c92923a98ce
Ganapati V S <vsg.inbox@gmail.com>
2019-09-07 20:29:19 +0530

    CSP fixes

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,7 +31,7 @@ function SEO() {
       />
       <meta
         httpEquiv="Content-Security-Policy"
-        content="default-src 'self' https://www.google.com/analytics https://twitter.com https://github.com https://stackoverflow.com https://www.linkedin.com https://dribbble.com https://www.instagram.com https://www.facebook.com https://www.google-analytics.com ; base-uri 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; block-all-mixed-content; connect-src 'self' ws: ; img-src 'self' https://www.google-analytics.com; manifest-src 'self'; font-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com;"
+        content="default-src 'self' https://www.google.com/analytics https://marketingplatform.google.com/about/analytics https://twitter.com https://github.com https://stackoverflow.com https://www.linkedin.com https://dribbble.com https://www.instagram.com https://www.facebook.com https://www.google-analytics.com ; base-uri 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; block-all-mixed-content; connect-src 'self' ws: ; img-src 'self' https://www.google-analytics.com; manifest-src 'self'; font-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com;"
       />
       <meta name="description" content={site.siteMetadata.description} />
       <meta name="robots" content="index,follow" />
