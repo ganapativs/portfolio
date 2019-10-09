9a07d9d1e98abd99710b5acafae426f7abadbffb
Ganapati V S <vsg.inbox@gmail.com>
2019-10-09 20:05:51 +0530

    Cleanup

--- src/components/hooks/useOutsideClick.js
+++ src/components/hooks/useOutsideClick.js
@@ -1,24 +1,16 @@
+// Detects clicks outside of the passed ref
 import { useEffect } from 'react';
 
-/**
- * Hook that alerts clicks outside of the passed ref
- */
-function useOutsideClick(ref, cb) {
-  /**
-   * Alert if clicked on outside of element
-   */
-  function handleClickOutside(event) {
-    if (ref.current && !ref.current.contains(event.target)) {
-      cb();
-    }
-  }
+function useOutsideClick(ref, onOutsideClick) {
   useEffect(() => {
-    // Bind the event listener
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onOutsideClick(event);
+      }
+    }
+
     document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   });
 }
 
