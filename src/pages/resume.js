0278ea5cdc732a43d323ddba38939a6e664053c4
Ganapati V S <vsg.inbox@gmail.com>
2019-12-22 16:38:19 +0530

    Use remote pdf worker

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Document, Page } from 'react-pdf';
+import { pdfjs, Document, Page } from 'react-pdf';
 import SizeMe from 'react-sizeme';
 import SEO from '../components/seo';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const ResumeWrapper = styled.div`
   flex: 1;
@@ -18,6 +20,10 @@ const ResumeDoc = styled.div`
 const Resume = ({ size: { width } }) => {
   /* eslint-disable no-underscore-dangle */
   const [theme, setTheme] = useState(window.__theme);
+  // This state is needed because the pdf js remote worker renders
+  // a white canvas for sometime, looks odd in dark mode.
+  // Only show pdf view after it's completely rendered.
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     const originalOnThemeChange = window.__onThemeChange;
@@ -25,6 +31,7 @@ const Resume = ({ size: { width } }) => {
     // Proxy
     window.__onThemeChange = () => {
       setTheme(window.__theme);
+      setVisible(false);
       originalOnThemeChange();
     };
 
@@ -47,12 +54,19 @@ const Resume = ({ size: { width } }) => {
             <button>Download resume</button>
           </a>
         </p>
-        <ResumeDoc>
+        <ResumeDoc
+          className="animated fadeIn"
+          style={{ visibility: visible ? 'visible' : 'hidden' }}>
           <Document
             file={`/resume/${theme}.pdf`}
             externalLinkTarget="_blank"
             loading={<div />}>
-            <Page width={width} pageNumber={1} loading={<div />} />
+            <Page
+              onRenderSuccess={() => setVisible(true)}
+              width={width}
+              pageNumber={1}
+              loading={<div />}
+            />
           </Document>
         </ResumeDoc>
       </div>
