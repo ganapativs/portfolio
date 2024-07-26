52ad76178eb06509aa79bd0d8e8148b4027372a3
ganapativs <vsg.inbox@gmail.com>
2024-07-27 00:12:02 +0530

    Hide color bar in mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 // TODO:
-// - Mobile header - clicking on not working when codesandbox iframe is below it
+// - Fix codesandbox links in blog
 // - Underline line/word on hover in blog (experimental)
 // - Code copy button - line no etc - https://www.gatsbyjs.com/plugins/gatsby-remark-prismjs-copy-button/
 // - Image modal in blog
